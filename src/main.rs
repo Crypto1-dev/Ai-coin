3a615ff4dc11a8cf6993c016336eabd9f5db1dbb
Crypto1-dev <empeyjj@icloud.com>
2025-04-21 17:14:56 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,11 @@
+use crate::blockchain::Block;
 
+pub const DIFFICULTY_PREFIX: &str = "0000";
+
+pub fn proof_of_work(mut block: Block) -> Block {
+    while !block.hash.starts_with(DIFFICULTY_PREFIX) {
+        block.nonce += 1;
+        block.hash = block.calculate_hash();
+    }
+    block
+}
