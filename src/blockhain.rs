187a3520ddf763d187e14cb2b51844d39c1f845b
Crypto1-dev <empeyjj@icloud.com>
2025-04-21 17:12:25 -0500

    Update blockhain.rs

--- src/blockhain.rs
+++ src/blockhain.rs
@@ -1,1 +1,42 @@
+use std::time::{SystemTime, UNIX_EPOCH};
 
+#[derive(Debug, Clone)]
+pub struct Block {
+    pub index: u64,
+    pub previous_hash: String,
+    pub timestamp: u128,
+    pub data: String,
+    pub hash: String,
+    pub nonce: u64,
+}
+
+impl Block {
+    pub fn new(index: u64, previous_hash: String, data: String) -> Self {
+        let timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis();
+        let mut block = Block {
+            index,
+            previous_hash: previous_hash.clone(),
+            timestamp,
+            data: data.clone(),
+            hash: String::new(),
+            nonce: 0,
+        };
+        block.hash = block.calculate_hash();
+        block
+    }
+
+    pub fn calculate_hash(&self) -> String {
+        let value = format!(
+            "{}{}{}{}{}",
+            self.index, self.previous_hash, self.timestamp, self.data, self.nonce
+        );
+        format!("{:x}", md5::compute(value))
+    }
+}
+
+pub fn create_genesis_block() -> Block {
+    Block::new(0, "0".to_string(), "Genesis Block".to_string())
+}
